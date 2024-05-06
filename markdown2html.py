c05f47c5d700707d5f566d553cfdccb1990895c6
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-06 22:47:58 +0100

    Markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,6 @@ import markdown
 
 def markdown_to_html(input_filename, output_filename):
     """Convert Markdown file to HTML"""
-    # if len(sys.argv) < 2:
-    #     sys.stderr.write(
-    #         "Usage: ./markdown2html.py <input_file> <output_file>\n")
-    #     sys.exit(1)
-    input_filename = sys.argv[1]
     output_filename = sys.argv[2]
     if not os.path.exists(input_filename):
         sys.stderr.write(f"Missing {input_filename}\n")
