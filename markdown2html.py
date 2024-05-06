7292c19166d9626f2169aacf8739188adcd46524
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-06 12:31:55 +0100

    Markdown to HTML

---
+++ markdown2html.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Markdown to HTML Converter"""
+import sys
+import os
+import markdown
+
+
+def markdown_to_html(input_filename, output_filename):
+    """Convert Markdown file to HTML"""
+    if len(sys.argv) < 2:
+        sys.stderr.write(
+            "Usage: ./markdown2html.py <input_file> <output_file>\n")
+        sys.exit(1)
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    if not os.path.exists(input_filename):
+        sys.stderr.write(f"Missing {input_filename}\n")
+        sys.exit(1)
+    with open(input_filename, 'r', encoding='utf-8') as f:
+        markdown_text = f.read()
+    html_text = markdown.markdown(markdown_text)
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        f.write(html_text)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    markdown_to_html(sys.argv[1], sys.argv[2])
