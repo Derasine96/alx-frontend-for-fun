e87ff237ce895a3f2e089edb904a40da0568baea
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-09 00:01:29 +0100

    Markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
+    markdown_to_html(sys.argv[1], sys.argv[2])
